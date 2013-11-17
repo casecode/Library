106d6d86d98f55905424362ee743724491977a1a
casecode <casey.r.white@gmail.com>
2013-11-17 13:57:04 -0600

    Updated Book class to add status and borrower, and added methods to call and update Book attributes.

--- library.rb
+++ library.rb
@@ -55,10 +55,56 @@ class Borrower
   end
 end
 
+# Public: Initializing a new book instance.
+#
+# title - Title of book to be added.
+# author - Author of book to be added.
+# @status - Set status to "available" when new book instance created.
+# @borrower - Borrower to be set when book checked in or out.
 class Book
   def initialize(title, author)
     @title = title
     @author = author
+    @status = "available"
+    @borrower = nil
     puts "#{@title} by #{@author} has been created."
+  end
+
+# Private: Adding methods to return title, author,
+#   status and borrower of book.
+  def title
+    @title
+  end
+
+  def author
+    @author
+  end
+
+  def status
+    @status
+  end
+
+  def borrower
+    @borrower
+  end
+
+# Public: Adding methods to update values of @status and @borrower.
+#
+# new_status - Sets new status of book (available/unavailble).
+# new_borrower - Sets borrower of book.
+#
+# Examples:
+#
+#   status=("unavailable")
+#
+#   borrower=("borrower_name")
+# 
+# Return new values of @status and @borrower.
+  def status=(new_value)
+    @status = new_value
+  end
+
+  def borrower=(new_value)
+    @borrower = new_value
   end
 end
